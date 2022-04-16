b24f0b49bf69bd4101547a9f56713ec7024355f9
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-04-16 18:01:28 +0700

    Update article.blade.php

--- resources/views/page/article.blade.php
+++ resources/views/page/article.blade.php
@@ -6,7 +6,7 @@
         <div class="highlight">
             <div class="testbg"></div>
             <div class="testbox">
-                <p>{{ $highlight->divisi->nama }}</p>
+                {{-- <p>{{ $article->divisi->nama }}</p> --}}
                 <h1>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h1>
                 <div class="date d-flex flex-row align-items-center">
                     <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
@@ -17,18 +17,21 @@
         <div class="recommended my-4">
             <h2>Articles</h2>
             <hr style="height: 2px; opacity: 100%" />
-            <div class="row mx-1">
-                @foreach($article as $key)
-                    <div class="thumbnail col-md-4 p-1">
+            <div class="row m-1">
+                <!-- div thumbnail-wrapper udah ready buat foreach() -->
+                @foreach($article as $key => $value)
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
                         <div class="description px-3">
-                            <p>{{ $key->divisi->nama }}</p>
-                            <h6>{{ $key->judul }}</h6>
+                            <p>{{ $value->divisi->nama }}</p>
+                            <h6>{{ $value->judul }}</h6>
                             <div class="date d-flex flex-row align-items-center">
                                 <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                                <p>{{ $key->waktu }}</p>
+                                <p>{{ Carbon\Carbon::parse($value->waktu)->format('d F Y') }}</p>
                             </div>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
