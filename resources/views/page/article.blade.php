f6744da2c90dd155437ce9df12eac789fcab44c4
ChristoAW <ChristoAW@users.noreply.github.com>
2022-04-16 10:18:25 +0700

    garis pagination crew oke + article ready foreach()

--- resources/views/page/article.blade.php
+++ resources/views/page/article.blade.php
@@ -6,8 +6,8 @@
 <div class="article">
     <div class="container">
         <div class="highlight">
-            <div class="testbg"></div>
-            <div class="testbox">
+            <div class="background"></div>
+            <div class="box">
                 <p>PROGRAMS</p>
                 <h1>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h1>
                 <div class="date d-flex flex-row align-items-center">
@@ -19,98 +19,113 @@
         <div class="recommended my-4">
             <h2>Articles</h2>
             <hr style="height: 2px; opacity: 100%" />
-            <div class="row mx-1">
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+            <div class="row m-1">
+                <!-- div thumbnail-wrapper udah ready buat foreach() -->
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row mx-1">
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row mx-1">
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="thumbnail col-md m-1">
-                    <div class="description px-3">
-                        <p>PROGRAM</p>
-                        <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
-                        <div class="date d-flex flex-row align-items-center">
-                            <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
-                            <p>February 21,2022</p>
+                <div class="thumbnail-wrapper col-md-4 p-1">
+                    <div class="thumbnail">
+                        <div class="description px-3">
+                            <p>PROGRAM</p>
+                            <h6>Manfaat Meminum Coklat Panas Untuk Kesehatan Tubuh</h6>
+                            <div class="date d-flex flex-row align-items-center">
+                                <img src="{{ asset('images/article/icons8-clock-24.png') }}" />
+                                <p>February 21,2022</p>
+                            </div>
                         </div>
                     </div>
                 </div>
